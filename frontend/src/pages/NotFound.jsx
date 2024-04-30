1563b74730e60a33ca8c4462e036314d0766480e
Quinnschohan <quinnschohan@gmail.com>
2024-04-29 20:51:30 -0400

    first commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+function NotFound() {
+  return <div>
+      <h1>404 Not Found Bro</h1>
+      <p>Apologies! The page you are trying to access cannot be found.</p>
+  </div>
+}
+
+export default NotFound
